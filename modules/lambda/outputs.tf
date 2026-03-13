ee16cbefab69332cf3f1017107995e9d45cd8365
FadekeF <fadekefashanu@bluelightcard.co.uk>
2026-03-13 08:46:52 +0000

    Test 1 (#10)
    * initial commit
    
    * merge with main
    
    * test version release
    
    * test version release
    
    * disable wiki
    
    * fix modules tflint
    
    * improve lambda module + add s3 module
    
    * test pre-commit fix
    
    * ignore examples directory in module versioning + switch for_each to count
    
    * ignore examples directory in module versioning + update readme
    
    * fix pre-commit docs
    
    * fix lambda module
    
    * fix variable
    
    * update lambda module
    
    * coalesce source code hash
    
    * add lambda outputs
    
    * switch from a list of functions to individual functions
    
    * fix naming
    
    * improve api gateway module
    
    * improve lambda outputs

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,29 +1,33 @@
-output "lambda_function_arn" {
+output "function_arn" {
   description = "ARN of the Lambda function."
   value       = aws_lambda_function.lambda_function.arn
 }
 
-output "lambda_function_name" {
+output "function_name" {
   description = "Name of the Lambda function."
   value       = aws_lambda_function.lambda_function.function_name
 }
 
-output "lambda_function_version" {
+output "function_invoke_arn" {
+  description = "Invoke ARN of the Lambda function."
+  value       = aws_lambda_function.lambda_function.invoke_arn
+}
+output "function_version" {
   description = "Version of the Lambda function."
   value       = aws_lambda_function.lambda_function.version
 }
 
-output "lambda_function_qualified_arn" {
+output "function_qualified_arn" {
   description = "Qualified ARN of the Lambda function (including version)."
   value       = "${aws_lambda_function.lambda_function.arn}:${aws_lambda_function.lambda_function.version}"
 }
 
-output "lambda_function_role_arn" {
+output "function_role_arn" {
   description = "ARN of the Lambda function execution role."
   value       = aws_lambda_function.lambda_function.role
 }
 
-output "lambda_function_vpc_config" {
+output "function_vpc_config" {
   description = "VPC configuration of the Lambda function (if applicable)."
   value       = aws_lambda_function.lambda_function.vpc_config
 }
